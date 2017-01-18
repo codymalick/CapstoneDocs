7d5b5bbe258c179ae87ee4461cdce73fa018cc82
Garrett Smith <garrett.smith13@gmail.com>
2017-01-18 12:37:13 -0800

    Pass data file as a command line option

--- network_graphs/iperfParser.rb
+++ network_graphs/iperfParser.rb
@@ -1,8 +1,26 @@
 #!/usr/bin/ruby
 require 'json'
 require'csv'
+require 'optparse'
 
 fileName = "testData.json"
+
+options = {}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: iperfParser.rb [options]"
+  opts.on("-f", "--file=FILE_INPUT", "file to read data from") do |f|
+    if(f)
+      fileName = f
+    end
+  end
+  opts.on("-h", "--help", "prints this help dialog") do |n|
+    puts opts
+    exit
+  end
+end.parse!
+
+
 file = File.read(fileName)
 
 dataHash = JSON.parse(file)
@@ -14,6 +32,5 @@ CSV.open("./bandwidth.csv", "wb") do |csv|
     retrans = interval["sum"]["retransmits"]
     csv << [bps, retrans]
   end
-
 end
 
